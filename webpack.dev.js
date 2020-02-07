7471416a43befbfa21dd23273165240189d05d69
vidban <vi.jain29@gmail.com>
2020-02-07 11:09:44 -0800

    fixed column autofit error in features

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -25,6 +26,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html"
